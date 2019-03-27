00750e638a75f3301fc71e6de4212ab04e995ec7
Ravensfolly <carolinepbreen@gmail.com>
2019-03-27 14:37:59 -0400

    Hi nathanielgit status!

--- src/Event.java
+++ src/Event.java
@@ -11,7 +11,7 @@ public abstract class Event
 		this.person = person;
 		this.date = date;
 	}
-	
+	//Hello there Nathaniel, Destroyer of Worlds and Minds and spelling sHUT UP BREEN
 	public String toString(Event anEvent)
 	{
 		String output = new String("Contact information: /n" + this.person.toString(this.person) + "/n Date of the meeting: " + this.date);
